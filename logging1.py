456e9d1e9e72eae403d1c2060b4fad7f0bbceb2c
zhangxiaolin <zhangxiaolin@gszh.cn>
2018-09-08 23:48:08 +0800

    添加日志

---
+++ logging1.py
@@ -0,0 +1,35 @@
+import logging
+import logging.handlers
+import os
+
+logDir = 'logs'
+if os.path.exists(os.getcwd() + '/' + logDir) is False:
+    os.mkdir(os.getcwd() + '/' + logDir)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter(fmt='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                              datefmt='%Y-%m-%d %H:%M:%S')
+
+# 写入文件，如果文件超过100个Bytes，仅保留5个文件。
+handler = logging.handlers.RotatingFileHandler(
+    logDir+'/app.log', maxBytes=2 * 1024 * 1024, backupCount=5)
+handler.setFormatter(formatter)
+handler.setLevel(logging.INFO)
+logger.addHandler(handler)
+'''日志输出到屏幕控制台'''
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+ch.setLevel(logging.INFO)
+logger.addHandler(ch)
+
+logger.critical('Critical Something')
+logger.error('Error Occurred')
+logger.warning('Warning exists')
+logger.info('Finished')
+logger.debug('Debugging')
+
+while True:
+    logger.info("sleep     sleepsleepsleepsleepsleepsleepsleepsleepsleepsleepsleeptest")
+    logger.critical('test critical')
