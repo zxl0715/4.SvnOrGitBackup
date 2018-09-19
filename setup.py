501b707f08df19f56c0288f5924b354d031613c4
zhangxiaolin <zhangxiaolin@gszh.cn>
2018-09-19 14:27:29 +0800

    pyinstaller  打包 添加 icon

--- setup.py
+++ setup.py
@@ -2,33 +2,19 @@
 import os.path
 import setuptools
 
-import svn
-
-_APP_PATH = os.path.dirname(svn.__file__)
-
-with open(os.path.join(_APP_PATH, 'resources', 'README.rst')) as f:
-    long_description = f.read()
-
-with open(os.path.join(_APP_PATH, 'resources', 'requirements.txt')) as f:
-    install_requires = list(map(lambda s: s.strip(), f.readlines()))
-
 setuptools.setup(
-    name='svn',
-    version=svn.__version__,
-    description="Intuitive Subversion wrapper.",
+    name='svn_or_git_backup',
+    version='1.0',
+    description="智恒公司 svn或git源代码备份归档操作",
     long_description='',
     classifiers=[],
-    keywords='svn subversion',
-    author='Dustin Oprea',
-    author_email='myselfasunder@gmail.com',
-    url='https://github.com/dsoprea/PySvn',
-    license='GPL 2',
-    packages=setuptools.find_packages(exclude=['tests']),
+    keywords='svn git backup',
+    author='zhangxiaolin',
+    author_email='zhangxioalin@gszh.cn',
+    url='www.gszh.cn',
+    packages=setuptools.find_packages(exclude=['venv']),
     include_package_data=True,
-    zip_safe=False,
-    package_data={
-        'svn': ['resources/README.rst',
-                'resources/requirements.txt'],
-    },
-    install_requires='python-dateutil>=2.2',
+    zip_safe=False ,
+    install_requires['svn','apscheduler','gitpython'] #定义依赖模块
+
 )
