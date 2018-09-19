0f354d99ae1510c9a51bca493b40d9fe69d458ab
zhangxiaolin <zhangxiaolin@gszh.cn>
2018-09-19 10:24:04 +0800

    优化

--- main.py
+++ main.py
@@ -189,18 +189,30 @@ def backup_file_svn():
 
 def backupCode(isZip=False):
     loggingHandler.logger.info('启动任务{}'.format(os.linesep))
-    # 拉取代码
-    backup_svn_git()
-    # 备份文件至svn备份服务器
-    backup_file_svn()
-    # 是否启用打包归档
-    if isZip:
-        # 按工程进行打包归档
-        make_compressed_file()
+    try:
+        # 拉取代码
+        loggingHandler.logger.debug('backup_svn_git 1')
+        backup_svn_git()
+        loggingHandler.logger.debug('backup_svn_git 2')
+        loggingHandler.logger.debug('backup_file_svn 1')
+        # 备份文件至svn备份服务器
+        backup_file_svn()
+        loggingHandler.logger.debug('backup_file_svn 2')
+
+        # 是否启用打包归档
+        if isZip:
+            # 按工程进行打包归档
+            loggingHandler.logger.debug('make_compressed_file 1')
+            make_compressed_file()
+            loggingHandler.logger.debug('make_compressed_file 2')
+
+    except Exception as e:
+        loggingHandler.logger.exception('备份任务出现异常')
     loggingHandler.logger.info('结束任务{}'.format(os.linesep))
 
 
 if __name__ == '__main__':
+    multiprocessing.freeze_support()  # 解决pyinstaller多进程打包问题
     loggingHandler.logger.info('{}————————————————————————————————————————————————{}'.format(os.linesep, os.linesep))
     loggingHandler.logger.info('启动程序运行！')
 
