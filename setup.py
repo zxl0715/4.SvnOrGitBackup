6b9afe44a46079a6fa90b6ff33e597026fde050e
zhangxiaolin <zhangxiaolin@gszh.cn>
2018-09-19 15:35:04 +0800

    pyinstaller  打包 添加 icon

--- setup.py
+++ setup.py
@@ -14,7 +14,21 @@ setuptools.setup(
     url='www.gszh.cn',
     packages=setuptools.find_packages(exclude=['venv']),
     include_package_data=True,
-    zip_safe=False ,
-    install_requires['svn','apscheduler','gitpython'] #定义依赖模块
+    zip_safe=False,
+    install_requires=['svn', 'apscheduler', 'gitpython']
 
 )
+# '''install_requires#定义依赖模块''''
+
+'''
+安装 
+#python setup.py build     # 编译
+
+#python setup.py install     #安装
+
+#python setup.py sdist       #源码安装包 生成压缩包(zip/tar.gz)
+
+#python setup.py bdist_wininst   #Windows 下使用 生成NT平台安装包(.exe)
+
+#python setup.py bdist_rpm #/Linux 下使用 生成rpm包
+'''
