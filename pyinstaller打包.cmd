dc85749e944d78dc04bbd8cb25b80a6db7ffc572
zhangxiaolin <zhangxiaolin@gszh.cn>
2018-09-19 14:32:28 +0800

    添加额外的资源

---
+++ pyinstaller打包.cmd
@@ -0,0 +1,6 @@
+@echo off
+E:
+cd E:\4.SvnOrGitBackup
+pyinstaller -w main.py -n SvnOrGitBackup -i favicon.ico -v 
+
+pause
---
+++ start.cmd
@@ -0,0 +1,4 @@
+@echo off
+c:
+cd C:\MyData\Work_ZH\WIS\100.project\4.SvnOrGitBackup
+python main.py
